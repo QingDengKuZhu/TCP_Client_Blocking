bebc17e44e72b2c01d88e5bda9eccd45c13f8d9b
QingDengKuZhu <qingdengkuzhu@foxmail.com>
2014-03-07 10:48:01 +0800

    主函数

---
+++ TCP_Client_Blocking/TCP_Client_Blocking/main.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include "my_function.h"
+
+#pragma comment(lib, "ws2_32.lib")
+
+using std::endl;
+using std::cout;
+using std::cin;
+
+int main(int argc, char *argv[])
+{
+	if (argc < 2)
+	{
+		return EXIT_FAILURE;
+	}
+	
+	const char* szServer = argv[1];
+	
+	int nPort = DEF_PORT;
+	if (argc >3)
+	{
+		nPort = atoi(argv[2]);
+	}
+
+	WSADATA wsadata;
+	if (SOCKET_ERROR == WSAStartup(MAKEWORD(2, 2), &wsadata))
+	{
+		cout << "WSAStartup error : " << WSAGetLastError() << endl;
+		return EXIT_FAILURE;
+	}
+
+
+	DoWork(szServer, nPort);
+
+	if (SOCKET_ERROR == WSACleanup())
+	{
+		cout << "WSAClearup error : " << WSAGetLastError() << endl;
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+
+
+}
